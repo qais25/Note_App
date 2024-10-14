0ec9a80a94f46135f47ff39f513c358e74da89c1
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-14 22:11:13 +0300

    add container

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -5,6 +5,8 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
